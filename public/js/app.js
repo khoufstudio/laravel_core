d79f8c41de4d84be49d5ef6acf1e16ef0199eb77
Rizqy Nurhaqy <rizqy.nurhaqy@gmail.com>
2019-11-12 08:46:59 +0700

    add font awesome

--- public/js/app.js
+++ public/js/app.js
@@ -49525,8 +49525,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/myprojects/LaraveCore/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/myprojects/LaraveCore/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/myprojects/laravel_core/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/myprojects/laravel_core/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
